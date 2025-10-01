66d3989686d8d4fe4ffabd44f5fd3b1d3bd199fa
Miguel TR <migueltr.2019@outlook.com>
2025-10-01 14:22:39 +0200

    first commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        float euros = 6;
+
+        System.out.printf("%.2f euros son %.0f pesetas", euros, euros*166.386);
     }
 }
